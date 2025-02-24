cb8ca8c6a992d59e916934a701fb8d50b6e72bcf
Jothebug <hayenbg2603@gmail.com>
2025-02-24 21:02:19 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by HaYen on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
