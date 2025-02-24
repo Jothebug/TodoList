1e917c027693fd44c3bdf0fa92862af8a94bc6ee
Jothebug <hayenbg2603@gmail.com>
2025-02-24 22:48:42 +0700

    implement ItemModel

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var  items: [String] = [
-        "This is the first title!",
-        "This is the second title!",
-        "This is the third title!"
+    @State var  items: [ItemModel] = [
+        ItemModel(title: "The first item ", isCompleted: false),
+        ItemModel(title: "The second item", isCompleted: false),
+        ItemModel(title: "The third item", isCompleted: true)
     ];
     
     var body: some View {
         List {
-            ForEach(items, id: \.self) {item in
-                ListRowView(title: item)
+            ForEach(items) {
+                ListRowView(item: $0)
             }
 
         }
